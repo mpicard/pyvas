a1e34eb95dc3bd33f41423dd176c54db72fa145b
Martin Picard <map@cix.ie>
2017-04-04 12:51:55 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.rst', 'r', 'utf-8') as f:
 
 setup(
     name="pyvas",
-    version="0.2.0",
+    version="0.3.0",
     description="An OpenVAS Managment Protocol (OMP) v7 client for Python",
     long_description=readme,
     url="https://github.com/mpicard/pyvas",
